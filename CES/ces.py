e9ba94b745d33295c52c387af731f1a00632decc
Jennifer Schweers <orange.jenny@gmail.com>
2024-02-20 20:35:49 -0500

    Moved remaining CES initial setup into CESData

--- CES/ces.py
+++ CES/ces.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from parents_politics_data import ParentsPoliticsData
@@ -10,10 +11,16 @@ class CESData(ParentsPoliticsData):
         all_waves = self._trim_columns_from_panel(panel)
         all_waves = self._add_cycle(all_waves)
         all_waves = self._add_age(all_waves)
-        return all_waves
+        all_waves = self._recode_issues(all_waves)
+        all_waves = self._consolidate_demographics(all_waves)
+        all_waves = self._add_income_brackets(all_waves)
+        return all_waves.copy()
 
     def _build_paired_waves(self, all_waves):
-        return all_waves
+        return pd.concat([
+            all_waves.assign(cycle=1012), # contains all data, but only look at 2010/2012
+            all_waves.assign(cycle=1214)  # contains all data, but only look at 2012/2014
+        ])
 
     def _trim_columns_from_panel(self, panel):
         # Drop most columns
@@ -68,3 +75,84 @@ class CESData(ParentsPoliticsData):
 
     def _add_age(self, all_waves):
         return all_waves.assign(age=lambda x: 2010 - x.birthyr_10)
+
+    def _recode_issues(self, all_waves):
+        # Recode a few columns to streamline later calculations
+        # TODO: Verify that all invalid vlues are NAed out
+        for year in (10, 12, 14):
+            # Recode guns to be continuous (swapping 2 and 3 so that "no change" is in the middle of "less strict" and "more strict")
+            label = f'CC{year}_320'
+            all_waves.loc[:, label] = np.where(all_waves[label] == 2, 3, np.where(all_waves[label] == 3, 2, np.where(all_waves[label] == 1, 1, np.nan)))
+
+            # Recode don't ask don't tell, swapping so that 1 is the more conservative value, to match gay marriage ban question
+            all_waves[f'CC{year}_330G'] = np.where(
+                all_waves[f'CC{year}_330G'] == 1,
+                2,
+                np.where(all_waves[f'CC{year}_330G'] == 2, 1, np.nan)
+            )
+
+            # Replace NA with 0 for child18num columns - seems this question was skipped if child18_{year} was No
+            label = f'child18num_{year}'
+            all_waves.loc[np.isnan(all_waves[label]), label] = 0
+
+            # CC10_414_1-CC10_414_6 are all usage of military for for different reasons: 1 yes, 2 no
+            # CC10_414_7 is a "none of the above" for the previous six: 1 yes, 2 no
+            all_waves[f'CC{year}_414_7'] = np.where(all_waves[f'CC{year}_414_7'] == 1, 2, 1)
+
+            # Flip the 2 yes/no immigration questions that are opposite polarity of the other 5
+            # For 1,7, 1 is more liberal and 2 is more conservative
+            # For 2,3,4,5,6, 1 is more conservative and 2 is more liberal
+            all_waves[f'CC{year}_322_1'] = np.where(all_waves[f'CC{year}_322_1'] == 1, 2, 1)
+            if year == 10:  # only asked in 2010
+                all_waves[f'CC{year}_322_7'] = np.where(all_waves[f'CC{year}_322_7'] == 1, 2, np.where(all_waves[f'CC{year}_322_7'] == 2, 1, np.nan))
+        return all_waves
+
+    # Consolidate demographics, arbitrarily using later data if there are differences
+    def _consolidate_demographics(self, all_waves):
+        for demo in ('gender', 'race', 'investor', 'newsint', 'educ', 'marstat', 'pew_religimp'):
+            old_labels = [f'{demo}_10', f'{demo}_12', f'{demo}_14']
+            all_waves[demo] = all_waves[old_labels].bfill(axis=1).iloc[:, 0]
+            all_waves.drop(old_labels, axis=1, inplace=True)
+        return all_waves
+
+    def _add_income_brackets(self, all_waves):
+        # Income: Start with faminc_14 because the buckets vary by year, and the 2014 buckets are more granular
+        # Income brackets are approximate, since incomes are given in ranges.
+        all_waves = all_waves.rename(columns={'faminc_14': 'income'})
+        all_waves = all_waves.assign(
+            income_quintile=lambda x:np.select(
+                [
+                    x.income == 1,
+                    x.income == 2,
+                    x.income == 3,
+                    x.income == 4,
+                    x.income == 5,
+                    x.income == 6,
+                    x.income == 7,
+                    x.income == 8,
+                    x.income == 9,
+                    x.income == 10, # note the 10 response could go into either 4th or 5th quintile
+                    x.income == 11,
+                    x.income == 12,
+                    x.income == 13,
+                    x.income == 14,
+                    x.income == 15,
+                    x.income == 16,
+                ],
+                [1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5],
+                default=np.NAN
+            ),
+            # "High income" is top 20% to match Reeves
+            high_income=lambda x: np.where(np.isnan(x.income_quintile), np.NAN, np.where(x.income_quintile == 5, 1, 0)),
+            # "Low income" is bottom 40%, to very roughly correspond with SCHIP eligibility
+            low_income=lambda x: np.select(
+                [
+                    np.isnan(x.income_quintile),
+                    x.income_quintile == 1,
+                    x.income_quintile == 2,
+                ],
+                [np.NAN, 1, 1],
+                default=0
+            ),
+        )
+        return all_waves
--- CES/new_child.py
+++ CES/new_child.py
@@ -10,91 +10,9 @@ CONTINUOUS_PREFIXES = set()
 CATEGORICAL_PREFIXES = set()
 
 ces_data = CESData()
-
 panel = ces_data.get_panel()
-
 three_years = ces_data.get_all_waves()
-
-# Recode a few columns to streamline later calculations
-# TODO: Verify thaat all invalid vlues are NAed out
-for year in (10, 12, 14):
-    # Recode guns to be continuous (swapping 2 and 3 so that "no change" is in the middle of "less strict" and "more strict")
-    label = f'CC{year}_320'
-    three_years.loc[:, label] = np.where(three_years[label] == 2, 3, np.where(three_years[label] == 3, 2, np.where(three_years[label] == 1, 1, np.nan)))
-
-    # Recode don't ask don't tell, swapping so that 1 is the more conservative value, to match gay marriage ban question
-    three_years[f'CC{year}_330G'] = np.where(
-        three_years[f'CC{year}_330G'] == 1,
-        2,
-        np.where(three_years[f'CC{year}_330G'] == 2, 1, np.nan)
-    )
-
-    # Replace NA with 0 for child18num columns - seems this question was skipped if child18_{year} was No
-    label = f'child18num_{year}'
-    three_years.loc[np.isnan(three_years[label]), label] = 0
-
-    # CC10_414_1-CC10_414_6 are all usage of military for for different reasons: 1 yes, 2 no
-    # CC10_414_7 is a "none of the above" for the previous six: 1 yes, 2 no
-    three_years[f'CC{year}_414_7'] = np.where(three_years[f'CC{year}_414_7'] == 1, 2, 1)
-
-    # Flip the 2 yes/no immigration questions that are opposite polarity of the other 5
-    # For 1,7, 1 is more liberal and 2 is more conservative
-    # For 2,3,4,5,6, 1 is more conservative and 2 is more liberal
-    three_years[f'CC{year}_322_1'] = np.where(three_years[f'CC{year}_322_1'] == 1, 2, 1)
-    if year == 10:  # only asked in 2010
-        three_years[f'CC{year}_322_7'] = np.where(three_years[f'CC{year}_322_7'] == 1, 2, np.where(three_years[f'CC{year}_322_7'] == 2, 1, np.nan))
-
-# Consolidate demographics, arbitrarily using later data if there are differences
-for demo in ('gender', 'race', 'investor', 'newsint', 'educ', 'marstat', 'pew_religimp'):
-    old_labels = [f'{demo}_10', f'{demo}_12', f'{demo}_14']
-    three_years[demo] = three_years[old_labels].bfill(axis=1).iloc[:, 0]
-    three_years.drop(old_labels, axis=1, inplace=True)
-
-# Income: Start with faminc_14 because the buckets vary by year, and the 2014 buckets are more granular
-# Income brackets are approximate, since incomes are given in ranges.
-three_years.rename(columns={'faminc_14': 'income'}, inplace=True)
-three_years = three_years.assign(
-    income_quintile=lambda x:np.select(
-        [
-            x.income == 1,
-            x.income == 2,
-            x.income == 3,
-            x.income == 4,
-            x.income == 5,
-            x.income == 6,
-            x.income == 7,
-            x.income == 8,
-            x.income == 9,
-            x.income == 10, # note the 10 response could go into either 4th or 5th quintile
-            x.income == 11,
-            x.income == 12,
-            x.income == 13,
-            x.income == 14,
-            x.income == 15,
-            x.income == 16,
-        ],
-        [1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5],
-        default=np.NAN
-    ),
-    # "High income" is top 20% to match Reeves
-    high_income=lambda x: np.where(np.isnan(x.income_quintile), np.NAN, np.where(x.income_quintile == 5, 1, 0)),
-    # "Low income" is bottom 40%, to very roughly correspond with SCHIP eligibility
-    low_income=lambda x: np.select(
-        [
-            np.isnan(x.income_quintile),
-            x.income_quintile == 1,
-            x.income_quintile == 2,
-        ],
-        [np.NAN, 1, 1],
-        default=0
-    ),
-)
-
-two_years = pd.concat([
-    three_years.assign(cycle=1012), # contains all data, but only look at 2010/2012
-    three_years.assign(cycle=1214)  # contains all data, but only look at 2012/2014
-])
-
+two_years = ces_data.get_paired_waves()
 
 ########################
 # Functions: utilities #
