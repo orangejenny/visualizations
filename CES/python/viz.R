cc6d0ee04c467588751d98d4e0df1db4e301e0d8
Jennifer Schweers <orange.jenny@gmail.com>
2024-05-02 19:18:07 -0400

    Updated aesthetics of covariate viz

--- CES/python/viz.R
+++ CES/python/viz.R
@@ -1,25 +1,32 @@
 library(ggplot2)
 
 # Set up sample data
-static_data <- tibble(
-  x = c("Alpha", "Alpha", "Alpha",
-        "Beta", "Beta", "Beta",
-        "Gamma", "Gamma", "Gamma",
-        "Delta", "Delta", "Delta",
-        "Epsilon", "Epsilon", "Epsilon"),
-  y = c(1, 3, 2, 1, 3, 4, 2, 6, 5, 6, 4, 7, 8, 9, 5),
-  z = c(1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3),
-  upp = c(1.2, 3.3, 2.1, 1.25, 3.3, 4.2, 2.2, 6.4, 5.3, 6.57, 4.15, 7.28, 8.6, 9.8, 5.2),
-  low = c(0.65, 2.1, 1.35, 0.58, 2.47, 3.15, 1.5, 5.34, 4, 4.8, 1.9, 6.2, 7, 8.19, 4.55)
+covariate_data <- tibble(
+  # Values are (pool, matched_set)
+  demographic = c(
+    "age", "age", "age",
+    "gender_1", "gender_1", "gender_1",
+    "gender_2", "gender_2", "gender_2",
+    "income", "income", "income",
+    "pew_churatd", "pew_churatd", "pew_churatd"
+  ),
+  value = c(1, 3, 2, 1, 3, 4, 2, 6, 5, 6, 1, 2, 3, 4, 5),
+  is_matched = c(
+    "treatment", "pool", "matched",
+    "treatment", "pool", "matched",
+    "treatment", "pool", "matched",
+    "treatment", "pool", "matched",
+    "treatment", "pool", "matched"
+  )
 )
 palette <- c("#009E73", "#0072B2", "#F0E442")
 
 # Dot plot
-ggplot(static_data,
-       aes(x = as_factor(x), y = y,
-           ymin = low, ymax = upp,
-           color = as_factor(z))) +
-  geom_pointrange(alpha = 0.8, position = position_dodge(width = 0.5), show.legend = FALSE) +
+ggplot(covariate_data,
+       aes(x = as_factor(demographic), y = value,
+           color = as_factor(is_matched))) +
+  geom_point(alpha = 0.8, size=5) +
+  scale_y_continuous(limits=c(0, 10)) +
   scale_colour_manual(values=palette) +
-  labs(x = "", y = "", title="Light and Breezy") +
+  labs(x = "", y = "", color="group", title="Covariate comparison of entire pool and matched set") +
   theme_minimal()
