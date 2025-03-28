135ee4d2553fd8f9e3b4b4383b94829b4b971f35
Jennifer Schweers <orange.jenny@gmail.com>
2025-03-28 16:08:24 -0400

    DRYed up descriptive table

--- asher/descriptive_classes.py
+++ asher/descriptive_classes.py
@@ -73,76 +73,34 @@ html = f'''
             <td>Floundering<br>(n={class_counts[FLOUNDERING]})</td>
         </tr>
         <!-- Demographic rows -->
-        <tr>
-            <td style="text-align: left;">Gender: Men</td>
-            <td>{categoricals['SEX'][ALL]['Male']}%</td>
-            <td>{categoricals['SEX'][FAINT]['Male']}%</td>
-            <td>{categoricals['SEX'][FLOURISHING]['Male']}%</td>
-            <td>{categoricals['SEX'][FLOUNDERING]['Male']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Race: Non-Hispanic White</td>
-            <td>{categoricals['RACEETHNICITY_dummy'][ALL][0]}%</td>
-            <td>{categoricals['RACEETHNICITY_dummy'][FAINT][0]}%</td>
-            <td>{categoricals['RACEETHNICITY_dummy'][FLOURISHING][0]}%</td>
-            <td>{categoricals['RACEETHNICITY_dummy'][FLOUNDERING][0]}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Education: Less than high school diploma</td>
-            <td>{categoricals['EDUCATION'][ALL]['Less than 12th grade, no diploma']}%</td>
-            <td>{categoricals['EDUCATION'][FAINT]['Less than 12th grade, no diploma']}%</td>
-            <td>{categoricals['EDUCATION'][FLOURISHING]['Less than 12th grade, no diploma']}%</td>
-            <td>{categoricals['EDUCATION'][FLOUNDERING]['Less than 12th grade, no diploma']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Education: High school diploma</td>
-            <td>{categoricals['EDUCATION'][ALL]['High school diploma (or equivalent)']}%</td>
-            <td>{categoricals['EDUCATION'][FAINT]['High school diploma (or equivalent)']}%</td>
-            <td>{categoricals['EDUCATION'][FLOURISHING]['High school diploma (or equivalent)']}%</td>
-            <td>{categoricals['EDUCATION'][FLOUNDERING]['High school diploma (or equivalent)']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Education: Some college</td>
-            <td>{categoricals['EDUCATION'][ALL]['Some education after high school, no degree']}%</td>
-            <td>{categoricals['EDUCATION'][FAINT]['Some education after high school, no degree']}%</td>
-            <td>{categoricals['EDUCATION'][FLOURISHING]['Some education after high school, no degree']}%</td>
-            <td>{categoricals['EDUCATION'][FLOUNDERING]['Some education after high school, no degree']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Education: College degree</td>
-            <td>{categoricals['EDUCATION'][ALL]['College degree (associate, bachelor’s, master’s, or doctorate)']}%</td>
-            <td>{categoricals['EDUCATION'][FAINT]['College degree (associate, bachelor’s, master’s, or doctorate)']}%</td>
-            <td>{categoricals['EDUCATION'][FLOURISHING]['College degree (associate, bachelor’s, master’s, or doctorate)']}%</td>
-            <td>{categoricals['EDUCATION'][FLOUNDERING]['College degree (associate, bachelor’s, master’s, or doctorate)']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Region: Midwest</td>
-            <td>{categoricals['region4'][ALL]['Midwest']}%</td>
-            <td>{categoricals['region4'][FAINT]['Midwest']}%</td>
-            <td>{categoricals['region4'][FLOURISHING]['Midwest']}%</td>
-            <td>{categoricals['region4'][FLOUNDERING]['Midwest']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Region: Northeast</td>
-            <td>{categoricals['region4'][ALL]['Northeast']}%</td>
-            <td>{categoricals['region4'][FAINT]['Northeast']}%</td>
-            <td>{categoricals['region4'][FLOURISHING]['Northeast']}%</td>
-            <td>{categoricals['region4'][FLOUNDERING]['Northeast']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Region: South</td>
-            <td>{categoricals['region4'][ALL]['South']}%</td>
-            <td>{categoricals['region4'][FAINT]['South']}%</td>
-            <td>{categoricals['region4'][FLOURISHING]['South']}%</td>
-            <td>{categoricals['region4'][FLOUNDERING]['South']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Region: West</td>
-            <td>{categoricals['region4'][ALL]['West']}%</td>
-            <td>{categoricals['region4'][FAINT]['West']}%</td>
-            <td>{categoricals['region4'][FLOURISHING]['West']}%</td>
-            <td>{categoricals['region4'][FLOUNDERING]['West']}%</td>
-        </tr>
+'''
+
+for (display_label, attr, key) in [
+    ("Gender: Men", "SEX", "Male"),
+    ("Race: Non-Hispanic White", "RACEETHNICITY_dummy", 0),
+    ("Education: Less than high school diploma", "EDUCATION", "Less than 12th grade, no diploma"),
+    ("Education: High school diploma", "EDUCATION", "High school diploma (or equivalent)"),
+    ("Education: Some college", "EDUCATION", "Some education after high school, no degree"),
+    ("Education: College degree", "EDUCATION", "College degree (associate, bachelor’s, master’s, or doctorate)"),
+    ("Region: Midwest", "region4", "Midwest"),
+    ("Region: Northeast", "region4", "Northeast"),
+    ("Region: South", "region4", "South"),
+    ("Region: West", "region4", "West"),
+    ("Income: $14,999 or less", "INCOME", "$14,999 or less"),
+    ("Income: $15,000 to $24,999", "INCOME", "$15,000 to $24,999"),
+    ("Income: $25,000 to $34,999", "INCOME", "$25,000 to $34,999"),
+    ("Income: $35,000 to $49,999", "INCOME", "$35,000 to $49,999"),
+    ("Income: $50,000 to $74,999", "INCOME", "$50,000 to $74,999"),
+    ("Income: $75,000 to $99,999", "INCOME", "$75,000 to $99,999"),
+    ("Income: $100,000 or over", "INCOME", "$100,000 or over"),
+]:
+    html += "<tr>"
+    html += f"""<td style="text-align: left;">{display_label}</td>"""
+    for class_id in [ALL, FAINT, FLOURISHING, FLOUNDERING]:
+        html += f"""<td>{categoricals[attr][class_id][key]}%</td>"""
+    html += "</tr>"
+
+html += f'''
         <tr>
             <td style="text-align: left;">Age</td>
             <td>{continuous['AGE'][ALL]['mean']} ({continuous['AGE'][ALL]['std']})</td>
@@ -156,55 +114,6 @@ html = f'''
             <td>${continuous['INCOME_cont'][FAINT]['mean']:,} (${continuous['INCOME_cont'][FAINT]['std']:,})</td>
             <td>${continuous['INCOME_cont'][FLOURISHING]['mean']:,} (${continuous['INCOME_cont'][FLOURISHING]['std']:,})</td>
             <td>${continuous['INCOME_cont'][FLOUNDERING]['mean']:,} (${continuous['INCOME_cont'][FLOUNDERING]['std']:,})</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $14,999 or less</td>
-            <td>{categoricals['INCOME'][ALL]['$14,999 or less']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$14,999 or less']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$14,999 or less']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$14,999 or less']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $15,000 to $24,999</td>
-            <td>{categoricals['INCOME'][ALL]['$15,000 to $24,999']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$15,000 to $24,999']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$15,000 to $24,999']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$15,000 to $24,999']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $25,000 to $34,999</td>
-            <td>{categoricals['INCOME'][ALL]['$25,000 to $34,999']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$25,000 to $34,999']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$25,000 to $34,999']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$25,000 to $34,999']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $35,000 to $49,999</td>
-            <td>{categoricals['INCOME'][ALL]['$35,000 to $49,999']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$35,000 to $49,999']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$35,000 to $49,999']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$35,000 to $49,999']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $50,000 to $74,999</td>
-            <td>{categoricals['INCOME'][ALL]['$50,000 to $74,999']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$50,000 to $74,999']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$50,000 to $74,999']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$50,000 to $74,999']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $75,000 to $99,999</td>
-            <td>{categoricals['INCOME'][ALL]['$75,000 to $99,999']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$75,000 to $99,999']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$75,000 to $99,999']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$75,000 to $99,999']}%</td>
-        </tr>
-        <tr>
-            <td style="text-align: left;">Income: $100,000 or over</td>
-            <td>{categoricals['INCOME'][ALL]['$100,000 or over']}%</td>
-            <td>{categoricals['INCOME'][FAINT]['$100,000 or over']}%</td>
-            <td>{categoricals['INCOME'][FLOURISHING]['$100,000 or over']}%</td>
-            <td>{categoricals['INCOME'][FLOUNDERING]['$100,000 or over']}%</td>
         </tr>
     </table>
 '''
